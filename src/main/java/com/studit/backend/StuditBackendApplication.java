7a758378bb7c2a7a304dcfdbdd670426678cca53
mtgv999 <petermgcz@gmail.com>
2025-02-27 17:24:47 +0900

    Restore deleted file

---
+++ src/main/java/com/studit/backend/StuditBackendApplication.java
@@ -0,0 +1,12 @@
+package com.studit.backend;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StuditBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StuditBackendApplication.class, args);
+	}
+
+}
